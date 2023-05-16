712440526f3108b27eeb9ab95251bdacc9eeecd4
Moin Ahamed <ahamedjust@gmail.com>
2023-05-16 16:07:49 +0530

    designing, adding items on html page when we click on add. and also updating the database in the firebase realtime database

--- add to cart/index.js
+++ add to cart/index.js
@@ -1,7 +1,63 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+  databaseURL: "https://playground-a64cc-default-rtdb.asia-southeast1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings); 
+const database = getDatabase(app);
+const shoppingListInDB = ref(database, "shoppingList"); 
+
 const addButton = document.getElementById('add-button');
 const inputField = document.getElementById('input-field');
+const shoppingList = document.getElementById('shopping-list');
+
 
 addButton.addEventListener('click', function() {
   let inputvalue = inputField.value; 
-  console.log(inputvalue);
+
+  push(shoppingListInDB, inputvalue);
+
+  clearInputField();
+
 });
+
+onValue(shoppingListInDB, function(snapshot) {
+  let itemsArray = Object.entries(snapshot.val());
+
+  console.log(snapshot.val());
+
+  clearShoppingList();
+
+  for(let i = 0; i < itemsArray.length; i++) {
+    let currentItem = itemsArray[i]; 
+    let currentItemID = currentItem[0]; 
+    let currenItemsValue = currentItem[1];
+
+    appenditem(currentItem);
+  }
+})
+
+
+function clearShoppingList() {
+  shoppingList.innerHTML = "";
+}
+
+function clearInputField() {
+  inputField.value = "";
+}
+
+function appenditem(item) { 
+
+  let itemID = item[0]; 
+  let itemValue = item[1];
+
+  let newEl = document.createElement('li'); 
+
+  newEl.textContent = itemID;
+
+  shoppingList.append(newEl);
+}
+
+
